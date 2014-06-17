07221810cb5567d474052f7337f6798c86bde3e1
Marko Lalic <marko.lalic@gmail.com>
2014-06-18 00:46:23 +0200

    Implement method for verifying RSA signatures
    The function is exposed through the ``crypto`` module of the chat
    app.  This low level verification function can be used by different
    clients later on for different message verification purposes, e.g.
    chat message signature verification, join chat room request signature,
    etc.

---
+++ tca/chat/crypto.py
@@ -0,0 +1,44 @@
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA
+
+import base64
+
+
+def verify(message, signature, public_key):
+    """
+    Verify whether the given signature of the message was produced using
+    the private key paired with the given public key.
+
+    :param message: A unicode string representing the message whose
+        signature is being verified
+    :param signature: The signature of the message represented as a base64
+        encoded bytearray
+    :param public_key: The public key to validate against, represented as
+        a base64 encoded bytearray
+    """
+    message = message.encode('utf-8')
+
+    try:
+        signature = base64.decodestring(signature)
+        public_key = base64.decodestring(public_key)
+    except:
+        # If either the signature or the public key cannot be converted
+        # to bytes (i.e. the base64 representation is invalid), indicate
+        # that the verification failed
+        return False
+    try:
+        public_key = RSA.importKey(public_key)
+    except:
+        # Invalid key => signature does not match it
+        return False
+
+    message_hash = SHA.new()
+    message_hash.update(message)
+
+    try:
+        verifier = PKCS1_v1_5.new(public_key)
+        return verifier.verify(message_hash, signature)
+    except:
+        # Error while verifying => invalid signature
+        return False
---
+++ tca/chat/tests/test_crypto.py
@@ -0,0 +1,124 @@
+"""
+Tests for the :mod:`chat.crypto` module which implements basic crypto
+functionality which is needed across different views.
+"""
+
+from django.test import TestCase
+
+from chat import crypto
+
+import json
+import os
+
+
+class VerifySignatureTestCase(TestCase):
+    """
+    Tests for the :func:`chat.crypto.verify` function of the
+    :mod:`chat.crypto` module.
+    """
+    @classmethod
+    def get_fixture_path(cls, fixture_file_name):
+        """Returns the path to a fixture files for this TestCase."""
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(
+            base_dir,
+            'fixtures',
+            fixture_file_name)
+
+    def setUp(self):
+        # Load message/signature fixtures
+        with open(self.get_fixture_path('message_fixtures.json')) as f:
+            self.message_fixtures = json.load(f)
+        # Set up a fixture public key
+        with open(self.get_fixture_path('pubkey.pub'), 'rb') as pubkey_file:
+            data = pubkey_file.read()
+        # Make sure the public key is unicode too
+        self.public_key = data.decode('utf-8')
+
+    def test_simple_message(self):
+        """
+        Tests that a simple (ASCII) message's signature is correctly
+        verified.
+        """
+        message = self.message_fixtures['simple-message']
+
+        result = crypto.verify(
+                message['text'],
+                message['signature'],
+                self.public_key)
+
+        self.assertTrue(result)
+
+    def test_message_unicode_easy(self):
+        """
+        Tests that a message containing non-ASCII German characters
+        is correctly verified.
+        """
+        message = self.message_fixtures['unicode-1']
+
+        result = crypto.verify(
+                message['text'],
+                message['signature'],
+                self.public_key)
+
+        self.assertTrue(result)
+
+    def test_message_unicode_korean(self):
+        """
+        Tests that a message containing non-ASCII Korean characters is
+        correctly verified.
+        """
+        message = self.message_fixtures['unicode-korean']
+
+        result = crypto.verify(
+                message['text'],
+                message['signature'],
+                self.public_key)
+
+        self.assertTrue(result)
+
+    def test_invalid_signature(self):
+        """
+        Tests that a message with an invalid signature is correctly
+        recognized.
+        """
+        message = self.message_fixtures['simple-message']
+        # Replace the first character with a different one making the
+        # signature invalid
+        message['signature'] = '*' + message['signature'][1:]
+
+        result = crypto.verify(
+            message['text'],
+            message['signature'],
+            self.public_key)
+
+        self.assertFalse(result)
+
+    def test_invalid_base64(self):
+        """
+        Tests that the message is not marked valid when the signature
+        is represented by an invalid base64 string.
+        """
+        message = self.message_fixtures['simple-message']
+        message['signature'] = 'asd'
+
+        result = crypto.verify(
+            message['text'],
+            message['signature'],
+            self.public_key)
+
+        self.assertFalse(result)
+
+    def test_invalid_public_key(self):
+        """
+        Tests that a message is not marked valid when the provided
+        public key is not valid.
+        """
+        message = self.message_fixtures['simple-message']
+
+        result = crypto.verify(
+            message['text'],
+            message['signature'],
+            'asd')
+
+        self.assertFalse(result)
