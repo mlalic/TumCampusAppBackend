6e3b1e77e1efb748a7cb47b959c85374284b26f5
Marko Lalic <marko.lalic@gmail.com>
2014-07-13 12:06:07 +0200

    Extend the tests of message endpoints
    Before doing any refactoring, it is imperative that there are tests in
    place so that existing functionality does not end up being broken.

--- tca/chat/tests/test_views.py
+++ tca/chat/tests/test_views.py
@@ -377,6 +377,11 @@ class MessageListTestCase(ViewTestCaseMixin, TestCase):
         self.assertEquals(message_count, len(response_content))
         for message, response_message in zip(messages, response_content):
             self.assertEquals(message.text, response_message['text'])
+            self.assertTrue(response_message['url'].endswith(
+                message.get_absolute_url()))
+            self.assertEquals(message.valid, response_message['valid'])
+            self.assertTrue(response_message['member'].endswith(
+                message.member.get_absolute_url()))
 
 
 class PublicKeyListTestCase(ViewTestCaseMixin, TestCase):
